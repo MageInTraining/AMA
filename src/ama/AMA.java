739f281f116fb168c1da53ea853263c9831d0bbd
cen62777 <mistehlik@csas.cz>
2019-07-25 16:37:33 +0200

    test of new branch commit

--- src/ama/AMA.java
+++ src/ama/AMA.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//AAAAAAAAAAAAAAAAAAAAAAAAAAA
 package ama;
 import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.FileNotFoundException;
