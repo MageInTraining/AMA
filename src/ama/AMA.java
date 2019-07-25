9335b63d10714c1d1c1891600a11338312de836a
cen62777 <mistehlik@csas.cz>
2019-07-25 10:46:48 +0200

    fcked up master scenario

--- src/ama/AMA.java
+++ src/ama/AMA.java
@@ -4,13 +4,10 @@
  * and open the template in the editor.
  */
 package ama;
-import static ama.Constants.EXP_PERCENTILE;
 import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import static java.lang.Math.log;
-import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Application;
@@ -21,7 +18,6 @@ import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 import org.apache.commons.math3.distribution.LogNormalDistribution;
-import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import org.apache.commons.math3.stat.descriptive.moment.Mean;
 
 /**
@@ -30,12 +26,6 @@ import org.apache.commons.math3.stat.descriptive.moment.Mean;
  */
 public class AMA extends Application{
     
-    /*
-    private static Double in1000years;
-    private static double x;
-    private static double mu;
-    private static double sigma;
-    */
     //TODO: fill maxExpectedRange from load
     private static int maxExpectedRange = 5000;
 
@@ -44,76 +34,71 @@ public class AMA extends Application{
         
         //TODO: give path to csv file as parametr
         String file_name= "C:\\Users\\cen62777\\Documents\\Rizika_test.csv";
-        PercentileSeeker pSeeker = new PercentileSeeker();        
-        List<Scenario> scenarios = new CsvToBeanBuilder(new FileReader(file_name)).withType(Scenario.class).withSeparator('\t').build().parse();
+        PercentileSeeker pSeeker = new PercentileSeeker();   
+        //creates list of scenarios, provided in csv file
+        List<Scenario> scenarios =
+                    new CsvToBeanBuilder(new FileReader(file_name)).
+                        withType(Scenario.class).withSeparator('\t').
+                            build().parse();
         
         //array of lognormal distributions for individual scenario 
-        double[] distribution = new double[(maxExpectedRange/10)+1];
-        
+        double[] distribution = new double[maxExpectedRange];
         //TODO: replace with method to simulate scenarios
         for (Scenario scenario : scenarios) {
             
-            System.out.println("Scenario " + scenario.getScenarioNumber());
-            System.out.println(scenario.getEstimated());
-            System.out.println(scenario.getProbability());
-            System.out.println(scenario.getMax());
-            
+            //creates lognormal distribution for the scenario
             scenario.setMu(log(scenario.getEstimated()));
             scenario.setSigma(pSeeker);
-            
-            System.out.println(scenario.getMu());
-            System.out.println(scenario.getSigma());
-            
-            //creates lognormal distribution for the scenario
-            LogNormalDistribution ln1 = new LogNormalDistribution(scenario.getMu(),
-                    scenario.getSigma());
+            LogNormalDistribution ln1 =
+                    new LogNormalDistribution(
+                            scenario.getMu(),scenario.getSigma());
             
             //simulates scenario in the span of 1000 years
             for(int i = 0; i < (scenario.getProbability() * 1000); i++ ){
                 Double d = ln1.sample();
-                //System.out.println(d);
-                
+
                 //replaces values from 0.991+ with value from 0.990
                 if(d.intValue()>scenario.getMax()){
                     d=(double)scenario.getMax();
                 }
+                //adds count to distribution slot
                 try{
-                    distribution[d.intValue()/10]++;
+                    distribution[d.intValue()]++;
                 }catch(Exception e){
-                    System.out.println("Scenario " + scenario.getScenarioNumber() +
-                            ": " + e);
+                    System.out.println("Scenario "
+                            + scenario.getScenarioNumber() + ": " + e);
                 }
             }
         }
-        
+        //creates "Master scenario", representing all scenarios
         Mean mean = new Mean();
+        Scenario master = new Scenario();
+        master.setEstimated((int)mean.evaluate(distribution));
+        master.setMax(maxExpectedRange);
+        master.setProbability(1.0);
+        master.setMu(log(master.getMax()));
+        master.setSigma(pSeeker);
         
-        StandardDeviation sd1 = new StandardDeviation();
+        double[] distribution2 = new double[maxExpectedRange];
         
-//        LogNormalDistribution ln2 = new LogNormalDistribution(5.1781, 0.2755);
-        LogNormalDistribution ln2 = new LogNormalDistribution(
-                log(mean.evaluate(distribution))
-                , pSeeker.getSigmaPerPercentile(
-                        0.99, maxExpectedRange, log(mean.evaluate(distribution)
-                        )
-                )
-        );
+        LogNormalDistribution ln2 =
+                    new LogNormalDistribution(
+                            master.getMu(),master.getSigma());
                                         
-        double[] distribution2 = new double[(maxExpectedRange/10)+1];
-        for(int i=0; i < 5000; i++){
-           Double d = ln2.sample();
-           //replaces values from 0.991+ with value from 0.990
-            if(d.intValue()>(maxExpectedRange/10)){
-                d=(double)(maxExpectedRange/10);
-            }
-            System.out.println("Master distribution "+ d);
+        for(int i=0; i < (master.getProbability()*1000); i++){
+            Double d = ln2.sample();
+            //replaces values from 0.991+ with value from 0.990
+            if(d.intValue()>master.getMax()){
+                d=(double)master.getMax();
+             }
             try{
-                distribution2[d.intValue()/10]++;
+                distribution2[d.intValue()]++;
             }catch(Exception e){
                 System.out.println("Master distribution " + e);
             }
         }
         
+        //TODO: move to separate class/method
         //Create graph of functions
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
@@ -127,7 +112,7 @@ public class AMA extends Application{
         
         for(int i=0;i < (maxExpectedRange/10)+1;i++){
             series.getData().add(new XYChart.Data(i, distribution[i]));
-            //series2.getData().add(new XYChart.Data(i, distribution2[i]));
+            series2.getData().add(new XYChart.Data(i, distribution2[i]));
         }
         Scene scene  = new Scene(lineChart,700,350);
         lineChart.setCreateSymbols(false);
