892533e71f11cb68d3e3037fd1d26ddc8eaf095b
cen62777 <mistehlik@csas.cz>
2019-08-28 15:25:25 +0200

    added loging of simalation to csv file, named "log_[category_name].csv"

--- src/ama/AMA.java
+++ src/ama/AMA.java
@@ -5,9 +5,12 @@
  */
 package ama;
 import static ama.Constants.NUMBER_OF_YEARS;
+import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import javafx.application.Application;
@@ -20,30 +23,30 @@ import javafx.stage.Stage;
 public class AMA extends Application{
 
     @Override
-    public void start(Stage stage) throws FileNotFoundException{
+    public void start(Stage stage) throws FileNotFoundException, IOException{
         
         //TODO: give path to csv file as parametr
         String file_name= "C:\\Users\\cen62777\\Documents\\Rizika_big.csv";
-        PercentileSeeker pSeeker = new PercentileSeeker();   
         //creates list of scenarios, provided in csv file
         List<Scenario> scenarios =
                     new CsvToBeanBuilder(new FileReader(file_name))
                         .withType(Scenario.class).withSeparator('\t')
                             .build().parse();
+        
         //individual list of scenarios for each of (Erste?) groups
-        Category fraud = new Category(0.6);
-        Category improperPractices= new Category(0.16);
-        Category infrastructure = new Category(0.16);
-        Category execution = new Category(0.5);
+        Category fraud = new Category("fraud", 0.6);
+        Category improperPractices= new Category("improperPractices", 0.16);
+        Category infrastructure = new Category("infrastructure", 0.16);
+        Category execution = new Category("execution", 0.5);
         Category notSet = new Category();
         
         for (Scenario scenario : scenarios){
             int s = scenario.getRiskType();
             double d = scenario.getMax();
-            //fucking up probability- Zahors bed
-            if(scenario.getProbability()>1.0){
-                scenario.setProbability(1.0);
-            }
+//            //Zahors bed
+//            if(scenario.getProbability()>1.0){
+//                scenario.setProbability(1.0);
+//            }
             switch(s) {
                 case 1:
                     fraud.addToList(scenario);
@@ -58,12 +61,14 @@ public class AMA extends Application{
                     }
                     break;
                 case 3:
-                    execution.addToList(scenario);
-                    if(execution.getMaxRange()<d){
-                        execution.setMaxRange(d);
+                    if(d>=execution.getThreshold()){
+                       execution.addToList(scenario);
+                       if(execution.getMaxRange()<d){
+                           execution.setMaxRange(d);
+                       }                       
                     }
                     break;
-                case 4:;
+                case 4:
                     improperPractices.addToList(scenario);
                     if(improperPractices.getMaxRange()<d){
                         improperPractices.setMaxRange(d);
@@ -96,14 +101,17 @@ public class AMA extends Application{
             }
         }
         scenarios.clear();
-        Category c = fraud;
-        c.calculateDistribution(pSeeker);
-//        GraphCreator gc = new GraphCreator();
-//        stage.setScene(gc.seScene(c));
-//        stage.show();
-        for(int i=0;i<5;i++){
-            System.out.println("Bucket " + (i + 1) +" : " + (NUMBER_OF_YEARS/(double)c.getDistribution(i)));
+        Category c = execution;
+        CSVWriter writer =
+                new CSVWriter(new FileWriter("C:\\Users\\cen62777\\Documents\\log_" + c.getCategoryName()+".csv"), '\t', '\0', '\0', "\n");
+        c.calculateDistribution(writer);
+        writer.close();
+        for(int i = 0; i < 5; i++){
+            System.out.println(" Bucket " + (i+1) +" : " + (NUMBER_OF_YEARS/(c.getBucket(i))));
         }
+        GraphCreator gc = new GraphCreator();
+        stage.setScene(gc.seScene(c));
+        stage.show();
     }
     
         /**
